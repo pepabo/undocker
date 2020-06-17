07417b79343fe0cd86bfdc8a0ef1c4f949fe84d7
k1LoW <k1lowxb@gmail.com>
2020-06-17 21:49:59 +0900

    Refactor TestImage_Extract

--- image_test.go
+++ image_test.go
@@ -8,59 +8,47 @@ import (
 )
 
 func TestImage_Extract(t *testing.T) {
-	// tokibi/busybox-bundle-registry
-	rtmpdir, err := ioutil.TempDir("", "registry")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(rtmpdir)
-	registry, err := NewRegistry("http://localhost:5000", "", "", rtmpdir)
-	if err != nil {
-		t.Error(err)
-	}
-	etmpdir, err := ioutil.TempDir("", "undocker")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(etmpdir)
-
-	type fields struct {
-		Source     Source
-		Repository string
-		Tag        string
-	}
-	type args struct {
-		dir              string
-		overwriteSymlink bool
-	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name             string
+		rURL             string
+		repo             string
+		tag              string
+		overwriteSymlink bool
+		wantErr          bool
 	}{
 		{
-			name: "ok",
-			fields: fields{
-				Source:     registry,
-				Repository: "busybox",
-				Tag:        "latest",
-			},
-			args: args{
-				dir:              etmpdir,
-				overwriteSymlink: false,
-			},
-			wantErr: false,
+			name: "Extract busybox from local registry",
+			// tokibi/busybox-bundle-registry
+			rURL:             "http://localhost:5000",
+			repo:             "busybox",
+			tag:              "latest",
+			overwriteSymlink: false,
+			wantErr:          false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			i := Image{
-				Source:     tt.fields.Source,
-				Repository: tt.fields.Repository,
-				Tag:        tt.fields.Tag,
+			rtmpdir, err := ioutil.TempDir("", "registry")
+			if err != nil {
+				t.Fatal(err)
 			}
-			if err := i.Extract(tt.args.dir, tt.args.overwriteSymlink); (err != nil) != tt.wantErr {
+			defer os.RemoveAll(rtmpdir)
+			etmpdir, err := ioutil.TempDir("", "undocker")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer os.RemoveAll(etmpdir)
+
+			registry, err := NewRegistry(tt.rURL, "", "", rtmpdir)
+			if err != nil {
+				t.Error(err)
+			}
+			i := Image{
+				Source:     registry,
+				Repository: tt.repo,
+				Tag:        tt.tag,
+			}
+			if err := i.Extract(etmpdir, tt.overwriteSymlink); (err != nil) != tt.wantErr {
 				t.Errorf("Image.Extract() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if info, err := os.Stat(filepath.Join(etmpdir, "bin")); err != nil || !info.IsDir() {
